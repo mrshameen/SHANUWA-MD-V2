06752797371219ff43a80039d686934fdf3f651e
SL Shanuwa <Shameendurage@gmail.com>
2024-07-01 00:11:53 +0530

    ├✤ 𝑺𝑯𝑨𝑵𝑼𝑾𝑨 𝑴𝑫 𝑽2 ✤┤

--- plugins/autoreact.js
+++ plugins/autoreact.js
@@ -2,7 +2,7 @@
 
 let handler = async (m, { conn, text }) => {
 
-    Conn.sendMessage(m.chat, {
+    conn.sendMessage(m.chat, {
           react: {
             text: `${pickRandom(['⚖'])}`,
             key: m.key,
